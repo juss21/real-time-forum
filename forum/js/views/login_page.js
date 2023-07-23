d09859277870093f89a587d25167e17bc8612735
kasepuu <joel.meeks@outlook.com>
2023-07-23 07:55:25 +0300

    placeholder hotfix

--- forum/js/views/login_page.js
+++ forum/js/views/login_page.js
@@ -10,7 +10,7 @@ export default function () {
     <br>
     <h1>Login page</h1>    
     <br>
-        <input type="text" placeholder="Username" name="login_id" id="login_id" value=""><br><br>
+        <input type="text" placeholder="Username or email" name="login_id" id="login_id" value=""><br><br>
         <input type="password" placeholder="Password" name="login_pw" id="login_pw" value=""><br>
         <br>
         <a id="ErrorBox"></a>
